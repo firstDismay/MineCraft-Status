daaf55ffc1b837c3f328449a6450819415d005ce
firstDismay <firstdismay@gmail.com>
2017-12-10 21:09:41 +0500

    Добавьте файлы проекта.

---
+++ MineCraftStatus/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineCraftStatus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MineCraftServer Test = new MineCraftServer("127.0.0.1", 25565);
+        }
+    }
+}
